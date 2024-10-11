1177f37162549fd14831cc4e017884f5ae9000ef
Antonio Estrada <aestr26@illinois.edu>
2024-10-11 14:28:17 -0500

    Login page framework

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 // script.js
-const menuButton = document.getElementById('menuButton');
-const navigationMenu = document.getElementById('navigationMenu');
-
-menuButton.addEventListener('click', () => {
-    navigationMenu.classList.toggle('show');
-});
